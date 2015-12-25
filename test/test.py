49c103c4b3b89a1ec4d746841870b1847d5f3173
Solomon Huang <kaichanh@gmail.com>
2015-12-25 17:08:56 +0800

    Add document strings

--- test/test.py
+++ test/test.py
@@ -1,16 +1,20 @@
 import unittest
 
 class TestStringMethods(unittest.TestCase):
+    '''Test class'''
     def test_upper(self):
+        '''make string all upper case'''
         self.assertEqual('foo'.upper(), 'FOO')
 
     def test_isupper(self):
+        '''to test string is all upper case'''
         self.assertTrue('FOO'.isupper())
         self.assertFalse('foo'.isupper())
         self.assertFalse('Foo'.isupper())
         self.assertTrue('BAR'.isupper())
 
     def test_split(self):
+        '''to split string'''
         s = 'hello world'
         self.assertEqual(s.split(), ['hello', 'world'])
         with self.assertRaises(TypeError):
